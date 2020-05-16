6a47ace21b4f9d17bbac9b221a74215a8c26c9d0
tiantc <chen.tian@hcl.com>
2020-05-16 19:10:51 +0800

    tune ssh agent

--- .config/git_ssh.sh
+++ .config/git_ssh.sh
@@ -2,11 +2,42 @@
 # echo "currently no git ssh sock needed!"
 echo "About to initialize ssh auth agent"
 
-# for ssh-agent to auth git automatically.
+echo "ssh_auth_sock:"
+echo "$SSH_AUTH_SOCK"
+function createSSHAgent() {
+    echo "about to ssh-agent"
+    eval `ssh-agent`
+    ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+}
+
 if [ ! -S ~/.ssh/ssh_auth_sock ]; then
-  eval `ssh-agent`
-  ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+  # call createSSHAgent functio
+  createSSHAgent
 fi
 export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
-ssh-add -l > /dev/null || ssh-add
+ssh-add -l || ssh-add
 
+ret=$?
+echo "ret: ${ret}"
+## if ret is greater than 1
+if [ "${ret}" -gt 1 ]; then
+        echo "no available ssh_auth_sock, will remove it"
+        rm -rfv ~/.ssh/ssh_auth_sock
+        echo "ssh_auth_sock deleted, will create a new one"
+        createSSHAgent
+        export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+        echo "ssh_auth_sock created, connecting to it...."
+        ssh-add -l || ssh-add
+        ret=$?
+        echo "ret: ${ret}"
+        if [ "${ret}" -gt 1 ]; then
+                echo "connect to ssh_agent failed! stop trying"
+        else
+                echo "connect to ssh_agent successfully!"
+        fi
+else
+## else the ret is  not greater than 1
+        echo "connect to ssh_agent successfully!"
+fi
+
+echo "after ssh-agent"
